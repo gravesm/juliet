3b8d08e735fc56c5efa369c7c28c5ed4d67f6243
Mike Graves <mgraves@mit.edu>
2013-07-31 13:17:32 -0400

    Implements alias editing for publishers.

--- app/controllers/entity_refs_controller.rb
+++ app/controllers/entity_refs_controller.rb
@@ -1,8 +1,13 @@
 class EntityRefsController < ApplicationController
 
   def create
-    @journal = Journal.find(params[:journal_id])
-    @entity_ref = @journal.entity_refs.new(params[:entity_ref])
+    if params.has_key?(:journal_id)
+      @refable = Journal.find(params[:journal_id])
+    else
+      @refable = Publisher.find(params[:publisher_id])
+    end
+
+    @entity_ref = @refable.entity_refs.new(params[:entity_ref])
     @entity_ref.ref_type = RefType.where("type_name = ?", "Alias").first
     if @entity_ref.save
         render :json => @entity_ref
