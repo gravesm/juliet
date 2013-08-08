604b23f836ac0f9105359d3b6a018225964495cf
Mike Graves <mgraves@mit.edu>
2013-08-08 16:28:59 -0400

    Moving edit form javascript to asset pipeline.

--- app/controllers/entity_refs_controller.rb
+++ app/controllers/entity_refs_controller.rb
@@ -10,6 +10,7 @@ class EntityRefsController < ApplicationController
     @entity_ref = @refable.entity_refs.new(params[:entity_ref])
     @entity_ref.ref_type = RefType.where("type_name = ?", "Alias").first
     if @entity_ref.save
+        @entity_ref["url"] = url_for([@refable, @entity_ref])
         render :json => @entity_ref
     else
         render :json => @entity_ref.errors, :status => :unprocessable_entity
