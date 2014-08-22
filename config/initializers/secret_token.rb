e7b9e56a152e7807e3a3d50ba0f1e08252a04ea3
Mike Graves <mgraves@mit.edu>
2014-08-22 10:10:25 -0400

    Addressing deprecation warnings

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Pubeng::Application.config.secret_token = 'a64f618ad572c1885b835d9d9b88e47dd495b8d226b9d2d6044ff592d386315c14897e0d9e549e2350f4dfc0bb569a9adf8a251608f586486b16bbfdd2fff15a'
+Pubeng::Application.config.secret_key_base = '22f21ac78ec3ab662e7717960501223553b82776f616e369cff6a039a59899e02a939a7c5592bd20ef51b977bf366300bcd3e92604e1c87e911ce8ca9d326143'
