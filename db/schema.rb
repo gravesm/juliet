6d7360144b2d387ee769b8dbcc3b1562fb1febbe
Mike Graves <mgraves@mit.edu>
2014-08-28 10:21:41 -0400

    Formatting changes

--- db/schema.rb
+++ db/schema.rb
@@ -9,30 +9,30 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130725170149) do
+ActiveRecord::Schema.define(version: 20130725170149) do
 
-  create_table "entity_refs", :force => true do |t|
+  create_table "entity_refs", force: true do |t|
     t.text     "refvalue"
     t.integer  "refable_id"
     t.string   "refable_type"
     t.integer  "ref_type_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "entity_refs", ["refable_id"], :name => "index_entity_refs_on_refable_id"
+  add_index "entity_refs", ["refable_id"], name: "index_entity_refs_on_refable_id"
 
-  create_table "journals", :force => true do |t|
+  create_table "journals", force: true do |t|
     t.integer  "publisher_id"
     t.text     "name"
     t.text     "note"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "policies", :force => true do |t|
+  create_table "policies", force: true do |t|
     t.text     "contact"
     t.text     "note"
     t.integer  "embargo"
@@ -43,23 +43,23 @@ ActiveRecord::Schema.define(:version => 20130725170149) do
     t.text     "message"
     t.integer  "policyable_id"
     t.string   "policyable_type"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "policies", ["policyable_id"], :name => "index_policies_on_policyable_id"
+  add_index "policies", ["policyable_id"], name: "index_policies_on_policyable_id"
 
-  create_table "publishers", :force => true do |t|
+  create_table "publishers", force: true do |t|
     t.text     "note"
     t.text     "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "ref_types", :force => true do |t|
+  create_table "ref_types", force: true do |t|
     t.string   "type_name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
